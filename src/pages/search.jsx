b3c8b8e481847738d31d7f8f21ff99f6f302a6c1
osadavc <osadavidath@gmail.com>
2021-11-05 12:37:08 +0530

    added auto complete

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -1,13 +1,67 @@
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import tw from "tailwind-styled-components";
 import ArrowLeft from "../components/icons/ArrowLeft";
+import axios from "axios";
 
 const Search = () => {
   const Router = useRouter();
 
+  const [pickup, setPickup] = useState({
+    value: "",
+    list: [],
+    isFocused: false,
+  });
+
+  const [drop, setDrop] = useState({
+    value: "",
+    list: [],
+    isFocused: false,
+  });
+
   const goBack = () => {
     Router.back();
   };
+
+  // c-spell:disable
+  const MAP_BOX_API_KEY =
+    "pk.eyJ1Ijoib3NhZGF0aGVjb2RlciIsImEiOiJja3Zsbzc4dDc5N2RyMnVzN3l5eDE2Y2xsIn0.Yi-PSKvQzgFU7sQAIcwzKA";
+  // c-spell:enable
+
+  useEffect(() => {
+    if (pickup.value) {
+      axios
+        .get(getMapBoxURL(pickup.value))
+        .then((response) => {
+          setPickup((prevPickup) => ({
+            ...prevPickup,
+            list: response.data.features.map((item) => item.place_name),
+          }));
+        })
+        .catch((err) => console.log(err));
+    } else {
+      setPickup((prevPickup) => ({ ...prevPickup, list: [] }));
+    }
+  }, [pickup.value]);
+
+  useEffect(() => {
+    if (drop.value) {
+      axios
+        .get(getMapBoxURL(drop.value))
+        .then((response) => {
+          setDrop((prevDrop) => ({
+            ...prevDrop,
+            list: response.data.features.map((item) => item.place_name),
+          }));
+        })
+        .catch((err) => console.log(err));
+    } else {
+      setDrop((prevDrop) => ({ ...prevDrop, list: [] }));
+    }
+  }, [drop.value]);
+
+  const getMapBoxURL = (input) =>
+    `https://api.mapbox.com/geocoding/v5/mapbox.places/${input}.json?worldview=cn&access_token=${MAP_BOX_API_KEY}`;
 
   return (
     <Wrapper>
@@ -23,8 +77,53 @@ const Search = () => {
         </FromToIcons>
 
         <InputBoxes>
-          <Input placeholder="Enter Pickup Location" />
-          <Input placeholder="Where To ?" />
+          <div className="relative w-full">
+            <Input
+              placeholder="Enter Pickup Location"
+              value={pickup.value}
+              onChange={(e) =>
+                setPickup((prevPickup) => ({
+                  ...prevPickup,
+                  value: e.target.value,
+                }))
+              }
+              onFocus={() =>
+                setPickup((prevPickup) => ({ ...prevPickup, isFocused: true }))
+              }
+            />
+            <SearchSuggestions
+              list={pickup}
+              setState={(item) => {
+                setPickup((prevPickup) => ({
+                  ...prevPickup,
+                  value: item,
+                  isFocused: false,
+                }));
+              }}
+            />
+          </div>
+          <div className="relative w-full">
+            <Input
+              placeholder="Where To ?"
+              value={drop.value}
+              onChange={(e) =>
+                setDrop((prevDrop) => ({ ...prevDrop, value: e.target.value }))
+              }
+              onFocus={() =>
+                setDrop((prevDrop) => ({ ...prevDrop, isFocused: true }))
+              }
+            />
+            <SearchSuggestions
+              list={drop}
+              setState={(item) => {
+                setDrop((prevDrop) => ({
+                  ...prevDrop,
+                  value: item,
+                  isFocused: false,
+                }));
+              }}
+            />
+          </div>
         </InputBoxes>
 
         <PlusIcon src="https://img.icons8.com/ios/50/000000/plus-math.png" />
@@ -40,6 +139,28 @@ const Search = () => {
       </ConfirmLocationContainer>
     </Wrapper>
   );
+};
+
+const SearchSuggestions = ({ list, setState }) => {
+  if (list.isFocused) {
+    return (
+      <div className="absolute bg-white z-50 w-full shadow-md px-2 cursor-pointer">
+        {list.list?.map((item, index) => (
+          <div
+            onClick={() => setState(item)}
+            key={index}
+            className="mb-1 border-b last:border-b-0"
+          >
+            <div>
+              <h3>{item}</h3>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  } else {
+    return <div></div>;
+  }
 };
 
 const Wrapper = tw.div`
@@ -99,6 +220,7 @@ const Input = tw.input`
   outline-none
   p-2
   border-none
+  w-full
 `;
 
 const PlusIcon = tw.img`
